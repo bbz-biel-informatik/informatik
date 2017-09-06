a93d6c3683e3abe55ade2e423b11869403ea70f4
Lukas_Skywalker <lukas.diener@hotmail.com>
2017-09-06 16:10:40 +0200

    add payment

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
 require 'sinatra/base'
+require 'stripe'
 
 class MyApp < Sinatra::Base
+  set :publishable_key, ENV['STRIPE_PUBLISHABLE_KEY']
+  set :secret_key, ENV['STRIPE_SECRET_KEY']
+  set :products, {
+    book: OpenStruct.new(amount: 3220, name: 'Informationstechnologie Grundlagen')
+  }
+
+  Stripe.api_key = settings.secret_key
+
   get '/' do
-    erb :index
+    redirect '/courses/01_intro_zahlensysteme'
+  end
+
+  get '/courses/:name' do |name|
+    erb "courses/#{name}".to_sym
   end
   
   get '/exercises.html' do
@@ -27,5 +40,30 @@ class MyApp < Sinatra::Base
 
   get '/:file' do |file|
     send_file file if file.end_with?(".json")
+  end
+
+  post '/payment/:product_name' do |product_name|
+    product = settings.products[product_name.to_sym]
+
+    @amount = product.amount
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :amount      => @amount,
+      :description => product.name,
+      :currency    => 'chf',
+      :customer    => customer.id,
+      :receipt_email => params[:stripeEmail]
+    )
+
+    erb :payment_ok
+  end
+
+  error Stripe::CardError do
+    env['sinatra.error'].message
   end
 end
